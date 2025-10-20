e21145119759c5a84127bc77cc1178be5c105795
Semhal tsehaye <semhal.tsehaye@reach-initiative.org>
2025-10-20 13:13:22 +0300

    rt3

---
+++ kha.R
@@ -0,0 +1,18 @@
+install.packages("devtools")
+library(devtools)
+devtools::install_github("impact-initiatives/cleaningtools")
+library(cleaningtools)
+??check_outliers
+check_outliers(
+  MSNA_2025_Pilot ,
+  uuid_column = "_uuid",
+  element_name = "checked_dataset",
+  kobo_survey = NULL,
+  kobo_choices = NULL,
+  cols_to_add_cleaning_log = NULL,
+  strongness_factor = 3,
+  minimum_unique_value_of_variable = NULL,
+  remove_choice_multiple = TRUE,
+  sm_separator = ".",
+  columns_not_to_check = NULL
+)
